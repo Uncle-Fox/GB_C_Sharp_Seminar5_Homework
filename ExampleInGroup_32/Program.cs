bd5eb0edaddb78e87993793b7e4b89955d74627b
Uncle-Fox <Nemogemut@gmail.com>
2023-10-01 10:56:09 +0300

    добавили решеине 32 задачи

---
+++ ExampleInGroup_32/Program.cs
@@ -0,0 +1,44 @@
+﻿/* Задача 32: Напишите программу замена элементов
+массива: положительные элементы замените на
+соответствующие отрицательные, и наоборот.
+[-4, -8, 8, 2] -> [4, 8, -8, -2] */
+
+int[] CreateArrayRndInt(int size, int min, int max)
+{
+    int[] arr = new int[size];
+    Random rnd = new Random();
+
+    for (int i = 0; i < size; i++)
+    {
+        arr[i] = rnd.Next(min, max + 1);
+    }
+
+    return arr;
+}
+
+void PrintArray(int[] arr)
+{
+    for (int i = 0; i < arr.Length; i++)
+    {
+        if (i < arr.Length - 1) Console.Write($"{arr[i]}, ");
+        else Console.Write($"{arr[i]}");
+    }
+}
+
+void ReplaceNegativeWithPositive(int[] array)
+{
+    for (int i = 0; i < array.Length; i++)
+    {
+        array[i] *= -1;
+    }
+}
+
+int[] array = CreateArrayRndInt(12, -9, 9);
+Console.Write("[");
+PrintArray(array);
+Console.WriteLine("]");
+
+ReplaceNegativeWithPositive(array);
+Console.Write(" -> [");
+PrintArray(array);
+Console.WriteLine("]");
