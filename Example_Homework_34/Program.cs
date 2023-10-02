017af09aa51677d3eb859cf65a63a1cf9b29e709
Uncle-Fox <Nemogemut@gmail.com>
2023-10-02 23:47:18 +0300

    добавили решение задачи ДЗ №34

--- Example_Homework_34/Program.cs
+++ Example_Homework_34/Program.cs
@@ -4,3 +4,44 @@
 массиве. 
 [345, 897, 568, 234] -> 2
 */
+
+int[] CreateArrayRndInt(int size, int min, int max)
+{
+    int[] arr = new int[size];
+    Random rnd = new Random();
+
+    for (int i = 0; i < size; i++)
+    {
+        arr[i] = rnd.Next(min, max + 1);
+    }
+
+    return arr;
+}
+
+void PrintArray(int[] arr)
+{
+    for (int i = 0; i < arr.Length; i++)
+    {
+        if (i < arr.Length - 1) Console.Write($"{arr[i]}, ");
+        else Console.Write($"{arr[i]}");
+    }
+}
+
+int CountEvenNumbers(int[] array)
+{
+    int count = 0;
+    for (int i = 0; i < array.Length; i++)
+    {
+        if(array[i] % 2 == 0) count++;
+    }
+    return count;
+}
+
+int[] array = CreateArrayRndInt(5, 100, 999);
+Console.Write("[");
+PrintArray(array);
+Console.Write("]");
+
+int result = CountEvenNumbers(array);
+Console.Write(" -> ");
+Console.Write($"{result}");
